31ac81c4fc5addaac046432977e0fbda594e9f88
sage-jordan <sagemjrodan@gmail.com>
2019-09-15 23:22:00 -0400

    added react router dom

---
+++ client/src/tester.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDom from ''
+import App from './app';
+
--- client/src/useStorageHook.js
+++
@@ -1,7 +0,0 @@
-import { useLocalStorage } from "./useLocalStorage";
-
-export const useStorageHook = (key, initialValue) => {
-  const [data, setData] = useLocalStorage(key, initialValue || {});
-  
-  return data;
-};
