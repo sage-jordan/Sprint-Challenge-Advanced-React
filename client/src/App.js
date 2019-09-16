c56e636c876f267b2e4c8bed43747df802849869
sage-jordan <sagemjrodan@gmail.com>
2019-09-15 21:42:19 -0400

    finished cards map

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
-import Card from 'semantic-ui-react';
+import { Card } from 'semantic-ui-react';
 
 function App() {
 
+  const [data, setData] = useState([]);
+
+  // UseEffect and axios call, set data to res.data
   useEffect(() => {
     axios.get('http://localhost:5000/api/players')
       .then(res => {
         console.log(res.data);
+        setData(res.data);
       })
       .catch(err => console.log(err));
-  });
+  }, []);
 
   return (
     <div className="App">
-      
+      {/* Map data array and return a card for each player  */}
+      {data.map((player) => {
+        return <Card 
+          key={player.id}
+          header={player.name}
+          meta={player.id}
+          description={player.country}
+          extra={player.searches}
+          />
+      })}
     </div>
   );
 }
