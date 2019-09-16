4b4207332fddd625a163e5f2044ace158e27f018
sage-jordan <sagemjrodan@gmail.com>
2019-09-15 23:42:01 -0400

    changes

--- client/src/tester.js
+++ client/src/tester.js
@@ -1,4 +1,10 @@
 import React from 'react';
-import ReactDom from ''
+import {render} from '@testing-library/react';
 import App from './app';
 
+test('renders without crashing', () => {
+    const container = render(<App/>);
+    console.log(container);
+}) 
+
+test('verify ')
