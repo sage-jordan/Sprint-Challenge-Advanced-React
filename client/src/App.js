d6a8e3fddaa5c22780df1539626d85d283075df0
sage-jordan <sagemjrodan@gmail.com>
2019-09-15 21:29:58 -0400

    added semantic

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Card from 'semantic-ui-react';
 
 function App() {
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/players')
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => console.log(err));
+  });
+
   return (
     <div className="App">
       
