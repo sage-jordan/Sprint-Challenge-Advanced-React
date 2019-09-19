6d9d6f1d09aa86781614ebffd3cfecfc8463a0e7
sage-jordan <sagemjrodan@gmail.com>
2019-09-18 22:26:46 -0400

    added @testing-library and flex-wrap

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from "@testing-library/react";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('renders without crashing', () => {
+  const container = render(<App/>);
+  console.log("Container:", container);
+}) 
+
+test('verify balls and strikes display', () => {
+  const container = render(<App/>);
+  const balls = container.getByText('Balls');
+  const strikes = container.getByText('Strikes');
+  console.log("Balls:", balls, "Strikes:", strikes);
 });
